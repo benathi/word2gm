64086e24da7bdb88aed9021cc0f499929b6d8ac1
Ben Athiwaratkun <ben.athiwaratkun@gmail.com>
2018-02-27 17:09:29 -0500

    adding script to compile skip-gram

---
+++ compile_skipgram_ops.sh
@@ -0,0 +1,4 @@
+TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
+TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
+g++ -std=c++11 -shared word2vec_ops.cc word2vec_kernels.cc -o word2vec_ops.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+# This will generate a file word2vec_ops.so
